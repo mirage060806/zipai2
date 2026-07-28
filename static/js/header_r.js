722392238291875e9615cc1aa7c445baf7747855
m <mirage060806@naver.com>
2026-07-28 16:51:27 +0900

    0728

--- static/js/header_r.js
+++ static/js/header_r.js
@@ -3,6 +3,13 @@ const header = document.querySelector("#header");
 const subBg = document.querySelector(".sub-bg");
 const depth2List = document.querySelectorAll(".depth2"); // 클래스이므로 전체 선택
 
+const btnHamburger = document.querySelector('.btn-hamburger');
+const btnClose = document.querySelector('.btn-close')
+const gnb = document.querySelector('.gnb');
+const menuOverlay = document.querySelector('.menu-overlay')
+
+
+
 // 마우스가 들어왔을 때 (mouseenter)
 header.addEventListener("mouseenter", function () {
   this.classList.add("active");
@@ -24,3 +31,24 @@ header.addEventListener("mouseleave", function () {
     depth2.classList.remove("active");
   });
 });
+
+// 햄버거 버튼 클릭 시 오픈 캔버스 메뉴 등장
+// 이벤트 작성 형식
+// 객체(변수).addEventListener('이벤트종류',함수)
+// 익명함수 -> 화살표 함수
+btnHamburger.addEventListener('click', function(){
+  // gnb가 오른쪽(right:-100%)에서 왼쪽(right:0)으로 등장
+  gnb.classList.add('active')
+  menuOverlay.classList.add('active')
+})
+
+btnClose.addEventListener('click', function(){
+  // gnb가 오른쪽(right:-100% = 제거)에서 왼쪽(right:0 = 추가)으로 등장
+  gnb.classList.remove('active')
+  menuOverlay.classList.remove('active')
+})
+menuOverlay.addEventListener('click', function(){
+  // gnb가 오른쪽(right:-100% = 제거)에서 왼쪽(right:0 = 추가)으로 등장
+  gnb.classList.remove('active')
+  menuOverlay.classList.remove('active')
+})
